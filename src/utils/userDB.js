b20cf8f150ef229bf5c2d97ac7f48ea37ce0faa4
hawiecode <sanchezhdn160@gmail.com>
2023-06-06 14:27:29 -0400

    Initial commit

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "dobriantr2d2",
+  password: "123456",
+};
+
+export const userDetails = {
+  username: "dobriantr2d2",
+  firstName: "Haward",
+  lastName: "Sanchez Niño",
+  email: "sanchezhdn160@gmail.com",
+};
